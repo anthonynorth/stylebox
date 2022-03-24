2a33a5cbd7daf65ea4728469dec67f8faa2c3f4f
Anthony North <anthony.jl.north@gmail.com>
2022-03-24 16:44:39 +1000

    add studio galaxy style

---
+++ R/mapbox_studio.R
@@ -0,0 +1,13 @@
+#' Mapbox studio styles
+#'
+#' Mapbox studio style URLs.
+#' @name mapbox_studio_styles
+#' @seealso <https://studio.mapbox.com/>
+NULL
+
+
+#' @describeIn mapbox_studio_styles Mapbox Studio Galaxy
+#' @export
+mapbox_studio_galaxy <- function() {
+  "mapbox://styles/qfes/ckmygva0j1rye17p7a24g3oov"
+}
